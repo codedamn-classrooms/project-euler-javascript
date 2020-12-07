70eeb8986c9ff3f8d65384456788d0073e90c6f6
Mehul Mohan <mehulmpt@gmail.com>
2020-12-07 06:26:52 +0530

    lab added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function combinatoricSelections(limit) {
+function pokerHands(arr) {
 
-  return 1;
+  return true;
 }
 
-combinatoricSelections(1000000);
+const testArr = [
+  '8C TS KC 9H 4S 7D 2S 5D 3S AC',
+  '5C AD 5D AC 9C 7C 5H 8D TD KS',
+  '3H 7H 6S KC JS QH TD JC 2D 8S',
+  'TH 8H 5C QS TC 9H 4D JC KS JS',
+  '7C 5H KC QH JD AS KH 4C AD 4S'
+];
 
-module.exports = combinatoricSelections;
+pokerHands(testArr);
+
+module.exports = pokerHands;
