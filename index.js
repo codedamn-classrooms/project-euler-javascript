7ffc4766beb39ef90b9adf8ee7b6a8043c310e1c
Mehul Mohan <mehulmpt@gmail.com>
2020-12-07 06:26:53 +0530

    lab added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function diophantineEquation() {
+function maximumPathSumII(triangle) {
 
   return true;
 }
 
-diophantineEquation();
+const testTriangle = [[3, 0, 0, 0],
+                      [7, 4, 0, 0],
+                      [2, 4, 6, 0],
+                      [8, 5, 9, 3]];
 
-module.exports = diophantineEquation;
+maximumPathSumII(testTriangle);
+
+module.exports = maximumPathSumII;
