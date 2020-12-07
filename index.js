6af4ce03abf019105c42b7085b6e01a7f010924a
Mehul Mohan <mehulmpt@gmail.com>
2020-12-07 06:26:49 +0530

    lab added

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-function maximumPathSumI(triangle) {
+function countingSundays(firstYear, lastYear) {
 
   return true;
 }
 
-const testTriangle = [[3, 0, 0, 0],
-                      [7, 4, 0, 0],
-                      [2, 4, 6, 0],
-                      [8, 5, 9, 3]];
+countingSundays(1943, 1946);
 
-maximumPathSumI(testTriangle);
-
-module.exports = maximumPathSumI;
+module.exports = countingSundays;
