01c19ee038ec008f609cb85d3e87e493dd4ab354
Mehul Mohan <mehulmpt@gmail.com>
2020-12-07 06:26:49 +0530

    lab added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function numberLetterCounts(limit) {
+function maximumPathSumI(triangle) {
 
   return true;
 }
 
-numberLetterCounts(5);
+const testTriangle = [[3, 0, 0, 0],
+                      [7, 4, 0, 0],
+                      [2, 4, 6, 0],
+                      [8, 5, 9, 3]];
 
-module.exports = numberLetterCounts;
+maximumPathSumI(testTriangle);
+
+module.exports = maximumPathSumI;
