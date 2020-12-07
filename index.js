e5d31fd5a539a8aca3582ef283957d819874e6fa
Mehul Mohan <mehulmpt@gmail.com>
2020-12-07 06:26:54 +0530

    lab added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function sqrtDigitalExpansion() {
+function pathSumTwoWays(arr) {
 
   return true;
 }
 
-sqrtDigitalExpansion();
+// Only change code above this line
 
-module.exports = sqrtDigitalExpansion;
+const testMatrix = [
+  [131, 673, 234, 103, 18],
+  [201, 96, 342, 965, 150],
+  [630, 803, 746, 422, 111],
+  [537, 699, 497, 121, 956],
+  [805, 732, 524, 37, 331]
+];
+
+pathSumTwoWays(testMatrix);
+
+module.exports = pathSumTwoWays;
